95fee978c3e31245df1aab4ba809d4ec4326c0e9
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-12 17:44:18 -0300

    Update script.js

--- Ej3/js/script.js
+++ Ej3/js/script.js
@@ -3,7 +3,11 @@ const container = document.getElementById("container");
 const inputParagraph = document.getElementById("inputParagraph");
 
 buttonAdd.addEventListener("click", () => {
-  // Escribe el código necesario aquí
-  
+  // Escribe el código dnecesario aquí
+  const addParagraph = document.createElement("p");
+  let content = document.createTextNode(inputParagraph.value)
+  addParagraph.appendChild(content);
+  container.appendChild(addParagraph);
   //
 });
+  
