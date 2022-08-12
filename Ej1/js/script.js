7b50efdf3dc804b81121e9838cd9287384c2ef3e
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-12 18:04:37 -0300

    Update script.js

--- Ej1/js/script.js
+++ Ej1/js/script.js
@@ -1,7 +1,7 @@
 const changeBackground = document.getElementById("changeBackground");
 
 changeBackground.addEventListener("click", () => {
-  // Escribe el código necesario aquí
+ document.getElementById("lorem").style.backgroundColor = "blue";   
   
   //
 });
